c126b6ac592712179d70a2de31ae77beaa1a7589
Tim Stevens <timstevens@hush.com>
2018-03-11 20:13:39 -0500

    another commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,91 @@
+gameActive = true;
 round = 1;
 userWins = 0;
 userLosses = 0;
 userGuessesRemn = 5;
 userGuessed = [];
 appChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
+appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
+console.log("appChoice " + appChoice);
 
-function playPsychic() {
+function gameUpdate() {
+    document.getElementById("round").innerHTML = round;
+    document.getElementById("userWins").innerHTML = userWins;
+    document.getElementById("userLosses").innerHTML = userLosses;
+    document.getElementById("userGuessed").innerHTML = userGuessed;
+    document.getElementById("userGuessesRemn").innerHTML = userGuessesRemn;
+};
+gameUpdate();
+
+function gameReset() {
+    round = 1;
+    userWins = 0;
+    userLosses = 0;
+    userGuessesRemn = 5;
+    userGuessed = [];
+    appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
+    console.log("appChoice " + appChoice);
+};
+
+// for (var i = 10; i < 10; i++) {
+if (gameActive) {
     document.onkeyup = function(getTheGuess) {
         var userGuess = getTheGuess.key;
-        appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
-        pushGuess();
-    }
-};
-
-function pushGuess() {
-    userGuessed.push(playPsychic.userGuess);
-    document.getElementById("guessed").innerHTML = playPsychic.userGuess;
-    checkGuess();
-};
-
-function checkGuess() {
-    if (playPsychic.userGuess !== playPsychic.appChoice && userGuessesRemn > 0)  {
+        userGuessed.push(userGuess);
+        console.log("userGuess " + userGuess);
         userGuessesRemn = userGuessesRemn - 1;
-        playPsychic();
-    }
-    else if (playPsychic.userGuess !== playPsychic.appChoice && userGuessesRemn == 0)  {
-        round++;
-	    userLosses = userLosses + 1;
-	    alert("Try not to be a loser in the next round. Be the Psychic.");
-        userGuessed = [];
-        userGuessesRemn = 5;
-        if (round < 10)  {
-            playPsychic();
+    
+        if (userGuess !== appChoice && userGuessesRemn > 0) {
+            gameUpdate();
         }
-        else if (round = 10) {
-            gameActive = false;
-            if (userWins > userLosses)  {
-                alert("You WON!! Great job, sport!");
-            }
-            else if (userWins < userLosses)  {
-                confirm("Some psychic you are.");
-                alert("Loser");
-            }
-        }
-    }
-	else if (playPsychic.userGuess === playPsychic.appChoice)  {
+        else if (userGuess !== appChoice && userGuessesRemn == 0) {
             round++;
-            userWins = userWins + 1;
-            alert("You won that round, but every blind squirrel finds an acorn.");
-            playPsychic();
+            userLosses++;
+            userGuessed = [];
+            userGuessesRemn = 5;
+            gameUpdate();
+            if (round === 10 && userWins < userLosses) {
+                confirm("Some psychic you are.");
+            }
+            else if (round === 10 && userWins > userLosses) {
+                confirm("You lost that round, but...");
+                alert("You WON the game!!!");
+                gameActive = false;
+            }
+            else if (round < 10) {
+            appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
+            console.log("appChoice " + appChoice);
+            alert("Try not to be a loser in the next round. Be the Psychic.");
+            }
+        }
+        else if (userGuess === appChoice) {
+            round++;
+            userWins++;
+            userGuessed = [];
+            userGuessesRemn = 5;
+            gameUpdate();
+            if (round < 10) {
+                appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
+                console.log("appChoice " + appChoice);
+                alert("You won that round, but every blind squirrel finds an acorn.");
+            }
+            else if (round === 10 && userWins < userLosses) {
+                alert("You won that round, but you lost the game");
+                confirm("Some psychic you are.");
+                gameActive = false;
+            }
+            else if (round === 10 && userWins > userLosses) {
+                alert("You WON!!!");
+                gameActive = false;
+            }
+        }
+        else {
+            alert("Refresh the page to try again");
+        }
     }
-
 };
-playPsychic();
-console.log(playPsychic.userGuess);
-console.log(userGuessed);
+
+
+
+
+
