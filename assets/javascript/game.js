b55d54a1ec73f67fdd28944044df2f9a0038e861
Tim Stevens <timstevens@hush.com>
2018-03-13 00:58:25 -0500

    and still another final commit for homework assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
-var gameActive = true;
-var round = 1;
-var userWins = 0;
-var userLosses = 0;
-var userGuessesRemn = 5;
-var userGuessed = [];
-var appChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
-var appChoice;
-appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
-console.log("appChoice " + appChoice);
+function gameReset() {
+    round = 1;
+    userWins = 0;
+    userLosses = 0;
+    userGuessesRemn = 5;
+    userGuessed = [];
+    appChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
+    appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
+    console.log("appChoice " + appChoice);
+    pageUpdate();
+};
+gameReset();
 
 function pageUpdate() {
     document.getElementById("round").innerHTML = round;
@@ -18,7 +20,6 @@ function pageUpdate() {
 };
 pageUpdate()
 
-
 function newRound() {
     appChoice = appChoices[Math.floor(Math.random() * appChoices.length)];
     console.log("appChoice " + appChoice);
@@ -28,65 +29,69 @@ function newRound() {
     pageUpdate();
 };
 
-
 document.onkeyup = function(getTheGuess) {
-    if (gameActive) {
-        var userGuess = getTheGuess.key;
-        userGuessed.push(userGuess);
-        console.log("userGuessed " + userGuessed);
-        userGuessesRemn--;
+    var userGuess = getTheGuess.key;
+    userGuessed.push(userGuess);
+    console.log("userGuessed " + userGuessed);
+    userGuessesRemn--;
 
-        if ((userGuess !== appChoice) && (userGuessesRemn > 0)) {
-            pageUpdate();
+    if ((userGuess !== appChoice) && (userGuessesRemn > 0)) {
+        pageUpdate();
+    }
+    else if ((userGuess !== appChoice) && (userGuessesRemn == 0)) {
+        userLosses++;
+        pageUpdate();
+
+        if (round < 10) {
+            alert("You lost that round, but keep trying.");
+            newRound();
         }
-
-        else if ((userGuess !== appChoice) && (userGuessesRemn == 0)) {
-            userLosses++;
-            pageUpdate();
-                
-            if (round < 10) {
-                alert("You lost that round, but keep trying.");
-                newRound();
+        else if (round === 10) {
+            alert("You lost that round");
+            gameActive = false;
+     
+            if (userLosses > userWins) {
+                alert("And you lost the game.");
+                alert("Press any key to play again");
+                gameReset();
             }
-
-            else if (round === 10) {
-                alert("You lost that round");
-                gameActive = false;
-                    
-                if (userLosses > userWins) {
-                    alert("And you lost the game.");
-                }
-
-                else if (userLosses < userWins) {
-                    alert("But you WON the game!!");
-                }
+            else if (userLosses < userWins) {
+                alert("But you WON the game!!");
+                alert("Press any key to play again");
+                gameReset();
             }
-        }
-
-        else if (userGuess === appChoice) {
-            userWins++;
-            
-            if (round < 10) {
-                alert("You won that round.");
-                newRound();
-            }
-
-            else if (round === 10) {
-                alert("You won that round");
-                gameActive = false;
-                    
-                if (userLosses > userWins) {
-                    alert("But you LOST the game!!");
-                }
-
-                else if (userLosses < userWins) {
-                    alert("And you WON the game!!");
-                }
+            else if (userLosses === userWins) {
+                alert("But you TIED the game!!");
+                alert("Press any key to play again");
+                gameReset();
             }
         }
     }
-    else {
-        alert("Thanks for playing. Refresh the page to play again.");
+    else if (userGuess === appChoice) {
+        userWins++;
+            
+        if (round < 10) {
+            alert("You won that round.");
+            newRound();
+        }
+        else if (round === 10) {
+            alert("You won that round");
+
+            if (userLosses > userWins) {
+                alert("But you LOST the game!!");
+                alert("Press any key to play again");
+                gameReset();
+            }
+            else if (userLosses < userWins) {
+                alert("And you WON the game!!");
+                alert("Press any key to play again");
+                gameReset();
+            }
+            else if (userLosses === userWins) {
+                alert("And you TIED the game!!");
+                alert("Press any key to play again");
+                gameReset();
+            }
+        }
     }
 };
-
